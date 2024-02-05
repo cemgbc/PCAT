5fd6241a1ca34943ef7770eb9bd9cf3dc04ee8f7
cemgbc <110987912+cemgbc@users.noreply.github.com>
2024-02-05 17:31:03 +0300

    Ready For Heroku

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController');
+
+const { constants } = require('http2');
 const app = express();
+// Connect DB
+mongoose
+  .connect('mongodb+srv://cemgb34:cem1234@cluster0.pdr2lfc.mongodb.net/pcat-db?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to DB'));
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-});
-const port = 3000;
+//ROUTES
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+
+const port = process.env.PORT || 5000 ;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatildi...`);
 });
